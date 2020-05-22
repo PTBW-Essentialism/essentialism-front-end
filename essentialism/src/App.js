a6bc9645db92247b343768fc488d5925d36e748b
Keenan <keenanhallas@gmail.com>
2020-05-21 20:22:20 -0500

    landing page planned out

--- essentialism/src/App.js
+++ essentialism/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Route, Link } from "react-router-dom";
+import Landing from "./components/main/Landing";
 
 function App() {
   return (
     <div className="App">
-
+      <Route exact path="/">
+        <Landing />
+      </Route>
+      <Route exact path="/signin">
+        <Landing />
+      </Route>
+      <Route exact path="/signup">
+        <Landing />
+      </Route>
     </div>
   );
 }
