3fc0a6c74f0ff2e2f67bfb0b75605373570db6b3
Keenan <keenanhallas@gmail.com>
2020-05-26 11:41:38 -0600

    dashboard component started with some general styling

---
+++ essentialism/src/components/main/Dashboard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import AppLogo from "./AppLogo";
+
+const DashboardContainer = styled.div`
+    width: 24%;
+    height: 100%;
+    background: #F8F8FF;
+    font-family: 'Vibur', cursive;
+    color: #8FCB9B;
+    border-right: 2px solid #8FCB9B;
+    display: flex;
+    flex-direction: column;
+`;
+
+const DashboardHeader = styled.div`
+    display: flex;
+    justify-content: center;
+    line-height: 0.2;
+    font-size: 1.25rem;
+    margin: 2%;
+`;
+
+const Dashboard = () => {
+    return(
+        <DashboardContainer>
+            <DashboardHeader>
+                <AppLogo>es</AppLogo>
+                <h2>essentialism</h2>
+            </DashboardHeader>
+        </DashboardContainer>
+    );
+}
+
+export default Dashboard;
