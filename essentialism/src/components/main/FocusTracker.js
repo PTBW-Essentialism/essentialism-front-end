378b17d8f392fba9310b3dc5904bd8805e6779fc
Keenan <keenanhallas@gmail.com>
2020-05-28 17:20:41 -0600

    worked on areas of focus component & minimal styling - still needs more work to add functionality

--- essentialism/src/components/main/FocusTracker.js
+++ essentialism/src/components/main/FocusTracker.js
@@ -9,16 +9,25 @@ const TrackerContainer = styled.div`
     color: #8FCB9B;
     border: 2px solid #8FCB9B;
     margin: 2%;
+
+    ${props => (props.type === "area" ? `
+        width: 30vw;
+        height: 30vw;
+    ` : null)}
 `
 const TrackerHeader = styled.div`
     font-size: 4vw;
     margin-top: 20%;
+
+    ${props => (props.type === "area" ? `
+
+    ` : null)}
 `;
 
 const FocusTracker = (props) => {
     return (
-        <TrackerContainer>
-            <TrackerHeader>{props.focus}</TrackerHeader>
+        <TrackerContainer type={props.type}>
+            <TrackerHeader type={props.type}>{props.focus}</TrackerHeader>
         </TrackerContainer>
     );
 }
--- essentialism/src/index.js
+++ essentialism/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import {createStore, applyMiddleware} from "redux";
-import {Provider} from "react-redux"
+import {Provider} from "react-redux";
 import Reducer from "./state/Reducer";
 import thunk from "redux-thunk";
 
