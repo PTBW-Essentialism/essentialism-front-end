72557b32c3fe7effb98accb834df5eec6cc849c7
Keenan <keenanhallas@gmail.com>
2020-06-01 12:25:04 -0600

    added feature to focus trackers allowing user to add an importance description

--- essentialism/src/components/main/FocusTracker.js
+++ essentialism/src/components/main/FocusTracker.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { handleChange } from "../onboarding/Register";
+import { CustomButton } from "./CustomButton";
 
 const TrackerContainer = styled.div`
     width: 17vw;
@@ -9,6 +11,8 @@ const TrackerContainer = styled.div`
     color: #8FCB9B;
     border: 2px solid #8FCB9B;
     margin: 2%;
+    display: flex;
+    flex-direction: column;
 
     ${props => (props.type === "area" ? `
         width: 30vw;
@@ -28,18 +32,45 @@ const TrackerHeader = styled.div`
     ` : null)}
 `;
 
-const FocusTracker = ({type, focus, importance}) => {
+const Importance = styled.p`
+    font-size: 2vw;
+    padding: 2%;
+`;
+
+const FocusTracker = (props) => {
+    const [formState, setFormState] = useState({
+        importance: ""
+    });
+
+    const [importance, setImportance] = useState(); //creating this slice of state just to get this working at the last minute. You would normally post importance to backend, which is then used in rendering the focus trackers
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setImportance(formState.importance);
+    }
+
     return (
-        <TrackerContainer type={type}>
-            <TrackerHeader type={type}>{focus}</TrackerHeader>
-            {/*importance ?
-                <div>{importance}<div> :
-                <label htmlFor="importance">
-                    In a few sentences, describe why the selected values are important to you. Focus on your thoughts/feelings, and don’t worry about spelling, grammar, or how well-written it is.
-                    <textarea
-                        id="importance"
-                    />
-                <label>*/}
+        <TrackerContainer type={props.type}>
+            <TrackerHeader type={props.type}>{props.focus}</TrackerHeader>
+            { props.type === "area" ?
+                importance ?
+                    <Importance>{importance}</Importance> :
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="importance">
+                            In a few sentences, describe why the selected values are important to you. Focus on your thoughts/feelings, and don’t worry about spelling, grammar, or how well-written it is.
+                            <br />
+                            <textarea
+                                id="importance"
+                                name="importance"
+                                value={formState.importance}
+                                onChange={e => {
+                                    handleChange(e, formState, setFormState);
+                                }}
+                            />
+                        </label>
+                        <CustomButton>Submit</CustomButton>
+                    </form>
+                : null}
         </TrackerContainer>
     );
 }
